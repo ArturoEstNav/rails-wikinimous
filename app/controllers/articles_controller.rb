cac10957b2541a86a54ac17ecfadfb18b64bcdc9
Arturo Estrada <arturoestradanav@gmail.com>
2020-07-09 18:28:15 -0500

    forms are still missing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def new
+    @article = Article.new(article_params)
+    @article.save
   end
 
   def create
+    @article = Article.new
   end
 
   def show
@@ -19,5 +24,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article)
   end
 end
